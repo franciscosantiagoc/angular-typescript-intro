5348837f8c17e87766aed3aee5736654d51b167a
Francisco Santiago de la Cruz <franciscosantiagoc@gmail.com>
2023-08-06 22:04:35 -0600

    resolviendo ejercicio de funciones en typescript

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import './style.css'
 
 //import './topics/02-object-interface.ts';
 
-import './topics/03-functions.ts'
+//import './topics/03-functions.ts'
+
+import './topics/04-homework-types.ts';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   texto de prueba
 `;
